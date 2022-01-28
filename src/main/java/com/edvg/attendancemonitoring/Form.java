5c2bb9f007bd4d6d4b17f9bf00a2d5f0dfa4a896
evguevarra <evguevarra08@gmail.com>
2022-01-28 20:41:52 +0800

    merged login and dashboard

--- src/main/java/com/edvg/attendancemonitoring/Form.java
+++ src/main/java/com/edvg/attendancemonitoring/Form.java
@@ -1,15 +1,20 @@
 package com.edvg.attendancemonitoring;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
+import java.io.IOException;
 import java.net.URL;
 
 public class Form {
@@ -55,6 +60,28 @@ public class Form {
     @FXML
     void forgotexit(MouseEvent event) {
         forgotpass.setStyle("-fx-text-fill: #898585;");
+    }
+
+    @FXML
+    void handleLoginBtn(ActionEvent event){
+        Parent root = null;
+        try {
+             root = FXMLLoader.load(App.class.getResource("dashboard.fxml"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+
+        stage.show();
+
+        Loginbtn.getScene().getWindow().hide();
+
+
+
     }
 
 
