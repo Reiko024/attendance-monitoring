7255a930137e7bfba7b28d98f3e7a81bfd720e37
evguevarra <evguevarra08@gmail.com>
2022-01-29 17:35:18 +0800

    added main frame

---
+++ src/main/java/com/edvg/attendancemonitoring/MainFrameController.java
@@ -0,0 +1,107 @@
+package com.edvg.attendancemonitoring;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFrameController {
+
+    @FXML
+    private ImageView Profile;
+
+    @FXML
+    private Label dashboardBtn;
+
+    @FXML
+    private AnchorPane displayPane;
+
+    @FXML
+    private Label employeesBtn;
+
+    @FXML
+    private Label manualBtn;
+
+    @FXML
+    private Label monitoringBtn;
+
+    @FXML
+    private Label qrBtn;
+
+    @FXML
+    void enteredDashBoardBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enteredEmployeesBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enteredManualBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enteredMonitoringBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enteredQrBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitedDashBoardBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitedEmployeesBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitedManualBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitedMonitoringBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitedQrBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleDashboardBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleEmployeesBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleManualBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMonitoringBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleQrBtn(MouseEvent event) {
+
+    }
+
+}
