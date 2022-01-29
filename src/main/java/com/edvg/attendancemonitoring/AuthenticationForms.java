99bcb6ee5c34a2ffd14eb9cf34e26192afbf9367
evguevarra <evguevarra08@gmail.com>
2022-01-29 19:58:51 +0800

    frames added to main frame

--- src/main/java/com/edvg/attendancemonitoring/AuthenticationForms.java
+++ src/main/java/com/edvg/attendancemonitoring/AuthenticationForms.java
@@ -75,7 +75,7 @@ public class AuthenticationForms {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.centerOnScreen();
-
+        stage.setMaximized(true);
         stage.show();
 
         loginBtn.getScene().getWindow().hide();
--- src/main/java/com/edvg/attendancemonitoring/MainFrameController.java
+++ src/main/java/com/edvg/attendancemonitoring/MainFrameController.java
@@ -1,17 +1,22 @@
 package com.edvg.attendancemonitoring;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFrameController implements Initializable {
+
+    String currentClickedOption = "dashboard";
 
     @FXML
     private ImageView Profile;
@@ -36,56 +41,75 @@ public class MainFrameController implements Initializable {
 
     @FXML
     void enteredDashBoardBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void enteredEmployeesBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void enteredManualBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void enteredMonitoringBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void enteredQrBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("dashboard"))
+        dashboardBtn.setTextFill(Color.rgb(255, 255, 255));
 
     }
 
     @FXML
     void exitedDashBoardBtn(MouseEvent event) {
-
+        if(!currentClickedOption.equals("dashboard"))
+        dashboardBtn.setTextFill(Color.rgb(48, 48, 47));
     }
 
     @FXML
-    void exitedEmployeesBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void exitedManualBtn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void exitedMonitoringBtn(MouseEvent event) {
-
+    void enteredQrBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("qr"))
+        qrBtn.setTextFill(Color.rgb(255, 255, 255));
     }
 
     @FXML
     void exitedQrBtn(MouseEvent event) {
-
+        if(!currentClickedOption.equals("qr"))
+        qrBtn.setTextFill(Color.rgb(48, 48, 47));
     }
 
     @FXML
+    void enteredEmployeesBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("employees"))
+        employeesBtn.setTextFill(Color.rgb(255, 255, 255));
+    }
+
+    @FXML
+    void exitedEmployeesBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("employees"))
+        employeesBtn.setTextFill(Color.rgb(48, 48, 47));
+    }
+
+    @FXML
+    void enteredMonitoringBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("monitor"))
+        monitoringBtn.setTextFill(Color.rgb(255, 255, 255));
+    }
+
+    @FXML
+    void exitedMonitoringBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("monitor"))
+        monitoringBtn.setTextFill(Color.rgb(48, 48, 47));
+    }
+
+    @FXML
+    void enteredManualBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("manual"))
+        manualBtn.setTextFill(Color.rgb(255, 255, 255));
+    }
+
+    @FXML
+    void exitedManualBtn(MouseEvent event) {
+        if(!currentClickedOption.equals("manual"))
+        manualBtn.setTextFill(Color.rgb(48, 48, 47));
+    }
+
+
+
+
+    @FXML
     void handleDashboardBtn(MouseEvent event) {
+
+        currentClickedOption = "dashboard";
+
+        loadDisplay("dashboard.fxml");
+
         dashboardBtn.setTextFill(Color.rgb(255, 255, 255));
         qrBtn.setTextFill(Color.rgb(48, 48, 47));
         employeesBtn.setTextFill(Color.rgb(48, 48, 47));
@@ -97,6 +121,9 @@ public class MainFrameController implements Initializable {
 
     @FXML
     void handleEmployeesBtn(MouseEvent event) {
+
+        currentClickedOption = "employees";
+
         dashboardBtn.setTextFill(Color.rgb(48, 48, 47));
         qrBtn.setTextFill(Color.rgb(48, 48, 47));
         employeesBtn.setTextFill(Color.rgb(255, 255, 255));
@@ -106,6 +133,9 @@ public class MainFrameController implements Initializable {
 
     @FXML
     void handleManualBtn(MouseEvent event) {
+
+        currentClickedOption = "manual";
+
         dashboardBtn.setTextFill(Color.rgb(48, 48, 47));
         qrBtn.setTextFill(Color.rgb(48, 48, 47));
         employeesBtn.setTextFill(Color.rgb(48, 48, 47));
@@ -115,6 +145,9 @@ public class MainFrameController implements Initializable {
 
     @FXML
     void handleMonitoringBtn(MouseEvent event) {
+
+        currentClickedOption = "monitor";
+
         dashboardBtn.setTextFill(Color.rgb(48, 48, 47));
         qrBtn.setTextFill(Color.rgb(48, 48, 47));
         employeesBtn.setTextFill(Color.rgb(48, 48, 47));
@@ -124,6 +157,11 @@ public class MainFrameController implements Initializable {
 
     @FXML
     void handleQrBtn(MouseEvent event) {
+
+        currentClickedOption = "qr";
+
+        loadDisplay("qr-frame.fxml");
+
         dashboardBtn.setTextFill(Color.rgb(48, 48, 47));
         qrBtn.setTextFill(Color.rgb(255, 255, 255));
         employeesBtn.setTextFill(Color.rgb(48, 48, 47));
@@ -131,8 +169,20 @@ public class MainFrameController implements Initializable {
         manualBtn.setTextFill(Color.rgb(48, 48, 47));
     }
 
+    public void loadDisplay(String ui){
+        Node node = null;
+        try{
+            node = FXMLLoader.load(getClass().getResource(ui));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        displayPane.getChildren().clear();
+        displayPane.getChildren().setAll(node);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         dashboardBtn.setTextFill(Color.rgb(255, 255, 255));
+        loadDisplay("dashboard.fxml");
     }
 }
